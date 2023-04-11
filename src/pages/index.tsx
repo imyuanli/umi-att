61074dc23c7b1e5edee1c7f4d52454c14b01d4ac
yuanli <2865437316@qq.com>
2023-04-11 14:23:27 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import {Button} from "antd";
+
+export default function HomePage() {
+    return (
+        <div>
+            <div className={'text-red-500 text-3xl'}>test</div>
+            <Button type={'primary'}>test</Button>
+        </div>
+    );
+}
